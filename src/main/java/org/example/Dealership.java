13d07b6aa4b7331ccddd4bea43a5c10648cb0576
kxsanders <kxsanders98@gmail.com>
2025-10-24 11:48:44 -0500

    Finished up Vehicle Class, set up methods for Dealership Class, completing Phase 1 of the Workshop

--- src/main/java/org/example/Dealership.java
+++ src/main/java/org/example/Dealership.java
@@ -2,8 +2,10 @@ package org.example;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Dealership {
+
     private String name;
     private String address;
     private String phone;
@@ -13,6 +15,56 @@ public class Dealership {
         this.name = name;
         this.address = address;
         this.phone = phone;
-        this.inventory = new ArrayList<>();
+        this.inventory = new ArrayList<>();  //instantiate
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max){
+        return null;
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType){
+        return null;
+    }
+
+    public List<Vehicle> getAllVehicles(){
+        return new ArrayList<>(inventory);
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+        //add in later
+    }
+
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 }
