5e486efd00482d54a0201a136056e91134ba8314
kxsanders <kxsanders98@gmail.com>
2025-10-24 16:50:59 -0500

    updated UserInterface Class

--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -8,10 +8,66 @@ public class UserInterface {
     private Scanner scanner = new Scanner(System.in);
 
     //Constructor
-    //Load dealership from file
-    //Make main method that runs menu
-    //1. Display Menu
-    //2. Read the input from the user
-    //3. Handle the option
-    //Process Methods
+    public UserInterface() {
+
+        //load dealership data directly here
+        DealershipFileManager fileManager = new DealershipFileManager();
+        dealership = fileManager.getDealership();
+
+        if(dealership == null){
+            System.out.println("Could not load. Please try again.");
+        }
+        else{
+            System.out.println("Dealership loaded successfully.");
+        }
+    }
+
+    //Run menu
+    public void display() {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("---MAIN MENU---");
+            System.out.println("1) Find vehicles within a price range");
+            System.out.println("2) Find vehicles by make/model");
+            System.out.println("3) Find vehicles by year range");
+            System.out.println("4) Find vehicles by color");
+            System.out.println("5) Find vehicles by mileage range");
+            System.out.println("6) Find vehicles by type (car, truck, SUV, van)");
+            System.out.println("7) List ALL vehicles");
+            System.out.println("8) Add a vehicle");
+            System.out.println("9) Remove a vehicle");
+            System.out.println("-----------------");
+            System.out.println("99) QUIT");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1": processGetByPriceRequest(); break;
+                case "2": //ADD
+                case "3": //ADD
+                case "4": //ADD
+                case "5": //ADD
+                case "6": //ADD
+                case "7": //ADD
+                case "8": //ADD
+                case "9": //ADD
+                case "99":
+                    System.out.println("Goodbye!");
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("Invalid. Please try again.");
+            }
+        }
+    }
+
+    //Process Methods for each menu option
+    private void processGetByPriceRequest(){
+        //ADD
+    }
+    private void processGetByMakeModelRequest(){
+        //ADD
+    }
+
 }
